e34aa5921f54260d540a4f117bc460fd00e0bbe0
Rajani <rajanipro@gmail.com>
2013-09-18 11:25:27 +0530

    Modified: Source/SDK/OpenIdConnect/ClientCredentials.cs

--- Source/SDK/OpenIdConnect/ClientCredentials.cs
+++ Source/SDK/OpenIdConnect/ClientCredentials.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace PayPal.OpenidConnect
+namespace PayPal.OpenIdConnect
 {
     public abstract class ClientCredentials
     {
